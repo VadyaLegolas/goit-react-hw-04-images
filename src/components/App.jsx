15504df034b1be93a36d294e5ef001dd76081d92
Vlad <vadya-85@mail.ru>
2024-02-09 02:42:33 +0100

    upd app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,37 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { ToastContainer } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
+import { Searchbar } from './Searchbar/Searchbar';
+import { ImageGallery } from './ImageGallery/ImageGallery';
+
+export class App extends Component {
+  state = {
+    query: '',
+  };
+
+  searchSubmit = query => {
+    this.setState({ query });
+  };
+
+  render() {
+    return (
+      <div className="app">
+        <Searchbar onSubmit={this.searchSubmit} />
+        <ImageGallery query={this.state.query} />
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="colored"
+        />
+      </div>
+    );
+  }
+}
