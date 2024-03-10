8b9c619bb49516ad07ca634a8b99324e3d193141
Vlad <vadya-85@mail.ru>
2024-03-10 14:39:35 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -29,33 +29,3 @@ export const Modal = ({ image, onClose }) => {
     modalRoot
   );
 };
-
-// export class Modal extends Component {
-//   componentDidMount = () => {
-//     window.addEventListener('keydown', this.handlePressESC);
-//   };
-
-//   componentWillUnmount = () => {
-//     window.removeEventListener('keydown', this.handlePressESC);
-//   };
-
-//   handlePressESC = e => {
-//     if (e.code === 'Escape') this.props.onClose();
-//   };
-
-//   handleBackdropClick = e => {
-//     if (e.currentTarget === e.target) this.props.onClose();
-//   };
-
-//   render() {
-//     const { largeImage, tag } = this.props.image;
-//     return createPortal(
-//       <Overlay onClick={this.handleBackdropClick}>
-//         <ModalWindow>
-//           <ModalImage src={largeImage} alt={tag} />
-//         </ModalWindow>
-//       </Overlay>,
-//       modalRoot
-//     );
-//   }
-// }
