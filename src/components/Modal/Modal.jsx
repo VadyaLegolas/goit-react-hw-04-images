753d36c847ab08169058f0a8c2f15ec7e9986914
Vlad <vadya-85@mail.ru>
2024-03-10 14:39:12 +0100

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,35 +1,61 @@
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { ModalImage, ModalWindow, Overlay } from './Modal.styled';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount = () => {
-    window.addEventListener('keydown', this.handlePressESC);
-  };
+export const Modal = ({ image, onClose }) => {
+  useEffect(() => {
+    const handlePressESC = e => {
+      if (e.code === 'Escape') onClose();
+    };
 
-  componentWillUnmount = () => {
-    window.removeEventListener('keydown', this.handlePressESC);
-  };
+    window.addEventListener('keydown', handlePressESC);
 
-  handlePressESC = e => {
-    if (e.code === 'Escape') this.props.onClose();
-  };
+    return () => {
+      window.removeEventListener('keydown', handlePressESC);
+    };
+  }, [onClose]);
 
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) this.props.onClose();
+  const handleBackdropClick = e => {
+    if (e.currentTarget === e.target) onClose();
   };
+  return createPortal(
+    <Overlay onClick={handleBackdropClick}>
+      <ModalWindow>
+        <ModalImage src={image.largeImage} alt={image.tag} />
+      </ModalWindow>
+    </Overlay>,
+    modalRoot
+  );
+};
 
-  render() {
-    const { largeImage, tag } = this.props.image;
-    return createPortal(
-      <Overlay onClick={this.handleBackdropClick}>
-        <ModalWindow>
-          <ModalImage src={largeImage} alt={tag} />
-        </ModalWindow>
-      </Overlay>,
-      modalRoot
-    );
-  }
-}
+// export class Modal extends Component {
+//   componentDidMount = () => {
+//     window.addEventListener('keydown', this.handlePressESC);
+//   };
+
+//   componentWillUnmount = () => {
+//     window.removeEventListener('keydown', this.handlePressESC);
+//   };
+
+//   handlePressESC = e => {
+//     if (e.code === 'Escape') this.props.onClose();
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) this.props.onClose();
+//   };
+
+//   render() {
+//     const { largeImage, tag } = this.props.image;
+//     return createPortal(
+//       <Overlay onClick={this.handleBackdropClick}>
+//         <ModalWindow>
+//           <ModalImage src={largeImage} alt={tag} />
+//         </ModalWindow>
+//       </Overlay>,
+//       modalRoot
+//     );
+//   }
+// }
