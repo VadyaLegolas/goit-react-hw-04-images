316285f60f092d4f7978489083a2621ec1229651
Vlad <vadya-85@mail.ru>
2024-03-10 16:45:44 +0100

    Update ImageGallery.jsx

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 import { toast } from 'react-toastify';
 import { Gallery } from './ImageGallery.styled';
@@ -7,11 +7,115 @@ import { Loader } from 'components/Loader/Loader';
 import { Button } from 'components/Button/Button';
 import { Modal } from 'components/Modal/Modal';
 
-export const ImageGallery = () => {
-  return (
-    <div>ImageGallery</div>
-  )
+export const ImageGallery = ({query}) => {
+  const [gallery, setGallery] = useState(null);
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isLoadMore, setIsLoadMore] = useState(false);
+  const [page, setPage] = useState(1);
+  const isFirstRender = useRef(true);
+
+  
+useEffect(() => {
+  if (isFirstRender.current) {
+    isFirstRender.current = false;
+    return
+  }
+  const photosApiService = new PhotosApiService();
+  const getPhotos = async (query, page, message = true) => {
+    try {
+  photosApiService.query = query;
+  photosApiService.page = page;
+  setIsLoadMore(false);
+  const galleryFetch = await photosApiService.fetchPhotos();
+  
+  if (galleryFetch.totalHits === 0) {
+    throw new Error(`Ничего не найдено по запросу "${query}"`);
+  }
+  if (message) {
+    toast.success(`Найдено ${galleryFetch.totalHits} картинок`);
+  }
+  console.log(gallery);
+
+//   if (gallery) {
+//     
+//     setGallery(prevState => {
+//       return {
+//           totalHits: galleryFetch.totalHits,
+//           hits: [...prevState.hits, ...galleryFetch.hits],
+        
+//       };
+//     });
+//   } else {
+//     setGallery({
+//       totalHits: galleryFetch.totalHits, hits: [...galleryFetch.hits] 
+//     });
+//   }
+  photosApiService.totalPages = Math.ceil(
+    galleryFetch.totalHits / photosApiService.perPage
+  );
+  if (photosApiService.totalPages > 1) {
+    setIsLoadMore(true);
+  }
+  if (photosApiService.page === photosApiService.totalPages) {
+    setIsLoadMore(false);
+  }
+} catch (err) {
+  setError(err.message);
+} finally {
+  setIsLoading(false);
 }
+};
+
+  // setIsLoading(true);
+  // setGallery(null);
+  // setPage(1);
+  getPhotos(query, page);
+}, [ query, page])
+
+  
+
+  const tongleModal = () => {
+    setShowModal(prevState => !prevState);
+  };
+
+  const addImageToModal = item => {
+    setSelectedImage(item);
+    tongleModal();
+  };
+
+  const loadMore = () => {
+    setPage(prevState => {
+      return prevState + 1;
+    });
+  };
+
+  return (
+    <>
+      {showModal && <Modal image={selectedImage} onClose={tongleModal} />}
+      {error && <h1>{error}</h1>}
+      {gallery && (
+        <Gallery className="gallery">
+          {gallery.hits.map(({ id, webformatURL, tags, largeImageURL }) => {
+            return (
+              <ImageGalleryItem
+                key={id}
+                image={webformatURL}
+                tag={tags}
+                largeImage={largeImageURL}
+                onClick={addImageToModal}
+              />
+            );
+          })}
+        </Gallery>
+      )}
+      {isLoading && <Loader />}
+      {isLoadMore && <Button onLoadMore={loadMore}>LoadMore</Button>}
+    </>
+  );
+};
 
 // export class ImageGallery extends Component {
 //   state = {
